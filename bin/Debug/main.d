abc30575bf61852f0a35d7a2ba0e71f15b547409
barzakouski <barzakouski@9cea4170-5b58-0401-21af-7b56d1acb07f>
2010-05-11 01:17:16 +0000

    cleanup

--- bin/Debug/main.d
+++
@@ -1,3 +0,0 @@
-/home/denim/work/utfc/bin/Debug/main.o:  \
- /home/denim/work/utfc/test/main.c \
-  /home/denim/work/utfc/test/../src/utf/utf.h
--- bin/Debug/utf.d
+++
@@ -1,2 +0,0 @@
-/home/denim/work/utfc/bin/Debug/utf.o: /home/denim/work/utfc/src/utf.c \
-  /home/denim/work/utfc/src/utf/utf.h
