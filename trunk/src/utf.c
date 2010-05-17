d07b1cea04027d61de48dcf53f8924774ac637c8
barzakouski <barzakouski@9cea4170-5b58-0401-21af-7b56d1acb07f>
2010-05-17 00:41:55 +0000

    refactoring, code point calc and utf16->utf8 transformation

--- trunk/src/utf.c
+++ trunk/src/utf.c
@@ -1,109 +1,123 @@
 #include "utf/utf.h"
 
 #include <stdio.h>
+#include <limits.h>
 
 int is_valid_utf16_atom( const uint16_t * chr )
 {
-	//http://unicode.org/faq/utf_bom.html#utf16-7
-	if ( *chr == 0xFFFF || *chr == 0xFFFE || ( *chr >= 0xFDD0 && *chr <= 0xFDEF ) ) 
+	//http://unicode.org/faq/utf_bom.html#utf16-7 
+	//if ( *chr == 0xFFFF || *chr == 0xFFFE || ( *chr >= 0xFDD0 && *chr <= 0xFDEF ) )  //this chars are not printable, but valid
+	//	return 1 == 2;
+	if ( ( *chr >= LEAD_SURROGATE_MIN  && *chr <= LEAD_SURROGATE_MAX ) && ( *(chr+1) < TRAIL_SURROGATE_MIN  || *(chr+1) > TRAIL_SURROGATE_MAX ) )
 		return 1 == 2;
-	if ( ( *chr >= 0xD800 && *chr <= 0xDBFF ) && ( *(chr+1) < 0xDC00 || *(chr+1) > 0xDFFF ) )
+	
+	if ( ( *chr >= TRAIL_SURROGATE_MIN && *chr <= TRAIL_SURROGATE_MAX ) && ( *(chr-1) < LEAD_SURROGATE_MIN  || *(chr-1) > LEAD_SURROGATE_MAX ) )
 		return 1 == 2;
-	if ( ( *chr >= 0xDC00 && *chr <= 0xDFFF ) && ( *(chr-1) < 0xD800 || *(chr-1) > 0xDBFF ) )
-		return 1 == 2;
+	
 	return 1 == 1;
 }
 
 
-unsigned int utf8_to_utf16( const uint8_t * source, uint16_t * dest, unsigned int dest_size )
+uint32_t utf16le_code_point( const uint16_t * chr )
+{
+	if ( *chr >= LEAD_SURROGATE_MIN ) //we have 2 word value
+	{
+		return ( chr[0] << 10 ) + chr[1] + SURROGAT_OFFSET;
+	}
+	else
+		return (uint32_t)(*chr);
+}
+
+uint32_t utf16be_code_point( const uint16_t * chr )
+{
+	if ( be_to_le(*chr) >= LEAD_SURROGATE_MIN ) //we have 2 word value
+	{
+		return ( be_to_le(chr[0]) << 10 ) + be_to_le(chr[1]) + SURROGAT_OFFSET;
+	}
+	else
+		return (uint32_t)(be_to_le(*chr));	
+}
+
+
+unsigned int utf8_to_utf16( const uint8_t * source, uint16_t * dest, unsigned int dest_size, const char to_be )
 {
 	//TODO
+	unsigned int offset = 0;
+	while ( *(source + offset) != UTF8_EOL )
+	{
+			
+	}
 	return 0;
 }
 
-unsigned int utf16le_to_utf8( const uint16_t * source, uint8_t * dest, const unsigned int dest_size, unsigned int * bytes_used )
+
+unsigned int utf16_to_utf8( const uint16_t * source, uint8_t * dest, const unsigned int dest_size, unsigned int * bytes_used, const char to_be )
 {
 	unsigned int cur_pos = 0;
 	unsigned int offset = 0;
+	uint32_t cp;
+	int i = 0;
+	
 	while ( 42 )
 	{
 		if ( !is_valid_utf16_atom( source+offset ) )
 			return RET_INVALID_UTF16; 
-		if ( source[offset] == '\00' )
+		if ( source[offset] == UTF16_EOL ) 
 			break;
-		if ( source[offset] < 0x80 && cur_pos < dest_size ) 
-			dest[cur_pos++] = source[offset];
-		else
-		if ( source[offset] < 0x800 && cur_pos + 1 < dest_size  )
-		{
-			dest[cur_pos++] = (uint8_t)((source[offset] >> 6)            | 0xc0);
-            dest[cur_pos++] = (uint8_t)((source[offset] & 0x3f)          | 0x80);
-		}
+		
+		if ( offset == UINT_MAX )
+			return RET_STRING_TOO_BIG; // omg, what was it?
+			
+		if ( to_be == 0 )
+			cp = utf16le_code_point( &source[offset++] );
 		else 
-		if ( source[offset] < 0x10000 && cur_pos + 2 < dest_size ) 
-		{              
-            dest[cur_pos++] = (uint8_t)((source[offset] >> 12)           | 0xe0);
-            dest[cur_pos++] = (uint8_t)(((source[offset] >> 6) & 0x3f)   | 0x80);
-            dest[cur_pos++] = (uint8_t)((source[offset] & 0x3f)          | 0x80);
-        }
-        else //TODO: two words sequence 
-        if ( cur_pos + 3 < dest_size )
-        {      
-            dest[cur_pos++] = (uint8_t)((source[offset] >> 18)           | 0xf0);
-            dest[cur_pos++] = (uint8_t)(((source[offset] >> 12) & 0x3f)  | 0x80);
-            dest[cur_pos++] = (uint8_t)(((source[offset] >> 6) & 0x3f)   | 0x80);
-            dest[cur_pos++] = (uint8_t)((source[offset] & 0x3f)          | 0x80);
-        }
-        else 
-        	return RET_BUFFER_OVERFLOW;
-        offset++;
+			cp = utf16be_code_point( &source[offset++] );
+		printf( "code point %X \n", cp );
+		if ( cp > 0xFFFF )
+			offset++;
+		
+		unsigned int rez = convert_utf16atom( cp, dest, dest_size, &cur_pos );
+		if ( rez != RET_OK )
+			return rez; 
+	   
 	}
-	dest[cur_pos] = 0x0000;
+	dest[cur_pos++] = UTF8_EOL;
 	*bytes_used = cur_pos; 
 	return RET_OK;	
 }
 
-unsigned int utf16be_to_utf8( const uint16_t * source, uint8_t * dest, const unsigned int dest_size, unsigned int * bytes_used )
+
+// internal function. supposes, that codepoint is valid utf16 value. do not call directly from your code
+// 
+unsigned int convert_utf16atom( uint32_t codepoint, uint8_t * dest, const unsigned int dest_size, unsigned int * cur_pos)
 {
-	unsigned int cur_pos = 0;
-	unsigned int offset = 0;
-	while ( 42 )
+	
+	if ( codepoint < 0x80 && *cur_pos < dest_size ) 
+		dest[(*cur_pos)++] = codepoint;
+	else
+	if ( codepoint < 0x800 && *cur_pos + 1 < dest_size  )
 	{
-		if ( !is_valid_utf16_atom( source+offset ) )
-			return RET_INVALID_UTF16; 
-		if ( source[offset] == '\00' )
-			break;
-		if ( be_to_le(source[offset]) < 0x80 && cur_pos < dest_size ) 										//one byte 
-			dest[cur_pos++] = be_to_le(source[offset]);
-		else
-		if ( be_to_le(source[offset]) < 0x800 && cur_pos + 1 < dest_size  ) 									//two bytes
-		{
-			dest[cur_pos++] = (uint8_t)((be_to_le(source[offset]) >> 6)            | 0xc0);
-            dest[cur_pos++] = (uint8_t)((be_to_le(source[offset]) & 0x3f)          | 0x80);
-           
-		}
-		else 
-		if ( be_to_le(source[offset]) < 0x10000 && cur_pos + 2 < dest_size) 
-		{              																// three bytes
-            dest[cur_pos++] = (uint8_t)((be_to_le(source[offset]) >> 12)           | 0xe0);
-            dest[cur_pos++] = (uint8_t)(((be_to_le(source[offset] >> 6)) & 0x3f)   | 0x80);
-            dest[cur_pos++] = (uint8_t)((be_to_le(source[offset] & 0x3f))          | 0x80);
-        
-        }
-        else 
-        if ( cur_pos + 3 < dest_size ) 
-        {      																	    // four bytes
-            dest[cur_pos++] = (uint8_t)((be_to_le(source[offset]) >> 18)           | 0xf0);
-            dest[cur_pos++] = (uint8_t)(((be_to_le(source[offset]) >> 12) & 0x3f)  | 0x80);
-            dest[cur_pos++] = (uint8_t)(((be_to_le(source[offset]) >> 6) & 0x3f)   | 0x80);
-            dest[cur_pos++] = (uint8_t)((be_to_le(source[offset]) & 0x3f)          | 0x80);
-        } 
-        else 
-        	return RET_BUFFER_OVERFLOW;
-        offset++;
-        
+		dest[(*cur_pos)++] = (uint8_t)(( codepoint >> 6 )            | 0xc0);
+        dest[(*cur_pos)++] = (uint8_t)(( codepoint & 0x3f )          | 0x80);
 	}
-	dest[cur_pos] = '\00';
-	*bytes_used = cur_pos; 
-	return RET_OK;	
-}
+	else 
+	if ( codepoint < 0x10000 && *cur_pos + 2 < dest_size ) 
+	{              
+    	dest[(*cur_pos)++] = (uint8_t)((  codepoint >> 12 )           | 0xe0);
+        dest[(*cur_pos)++] = (uint8_t)((( codepoint >> 6 ) & 0x3f)    | 0x80);
+        dest[(*cur_pos)++] = (uint8_t)((  codepoint & 0x3f )          | 0x80);
+     }
+     else
+     if ( *cur_pos + 3 < dest_size )
+     {      
+     	dest[(*cur_pos)++] = (uint8_t)((  codepoint >> 18)           | 0xf0);
+        dest[(*cur_pos)++] = (uint8_t)((( codepoint >> 12) & 0x3f)   | 0x80);
+        dest[(*cur_pos)++] = (uint8_t)((( codepoint >> 6) & 0x3f)    | 0x80);
+        dest[(*cur_pos)++] = (uint8_t)((  codepoint & 0x3f)          | 0x80);
+     }
+     else 
+     	return RET_BUFFER_OVERFLOW;
+	return RET_OK;
+} 
+
+
